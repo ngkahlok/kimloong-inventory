cf386a3686b4ed783631bbdfa8996e2534cc4b82
Ng Kah Lok <ngkahlok0407@gmail.com>
2026-05-08 20:11:51 +0800

    Connect Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Dashboard from "@/components/Dashboard/Dashboard";
+import { createClient } from '@/utils/supabase/server';
+import { cookies } from 'next/headers';
 
 export default function Home() {
   return <Dashboard />;
